9722010ea2806f03b5553a264fd1da48f817f5ad
Marko Nađ <marko.nad123@gmail.com>
2025-07-10 20:16:40 +0200

    ECR test - Log requests.

--- src/main/java/com/virtualpairprogrammers/api/controllers/VehicleController.java
+++ src/main/java/com/virtualpairprogrammers/api/controllers/VehicleController.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,7 +24,10 @@ import com.virtualpairprogrammers.api.services.PositionTrackingExternalService;
 @RequestMapping("/")
 @CrossOrigin(origins = "*")
 public class VehicleController 
-{	
+{
+
+	private final Logger logger = LoggerFactory.getLogger(VehicleController.class);
+
 	@Autowired
 	private PositionTrackingExternalService externalService;
 	
@@ -42,6 +47,7 @@ public class VehicleController
     @CrossOrigin(origins = "*")
 	public Collection<LatLong> getHistoryFor(@PathVariable("vehicleName") String vehicleName)
 	{
+        logger.info("Getting history for vehicle {}", vehicleName);
 		Collection<LatLong> results = new ArrayList<>();
 		Collection<VehiclePosition> vehicles = externalService.getHistoryFor(vehicleName);
 		for (VehiclePosition next: vehicles)
@@ -58,7 +64,6 @@ public class VehicleController
     @CrossOrigin(origins = "*")
     public Collection<VehiclePosition> getAllVehiclePositions()
     {
-    	Collection<VehiclePosition> results = externalService.getAllPositions();
-    	return results;
+        return externalService.getAllPositions();
     }
 }
